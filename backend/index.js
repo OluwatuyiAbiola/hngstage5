ce86219322cee1a62777ffaaf879d65f8f62c188
Oluwatuyi Abiola <abiolaoluwatuyi@gmail.com>
2023-09-28 23:17:39 +0100

    backend

---
+++ backend/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+// Set up multer for video uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+// Define a route for video uploads
+app.post('/upload', upload.single('video'), (req, res) => {
+    if(err){
+        res.json({message: err});
+    } else{
+        res.status(200).json({ message: 'Video uploaded successfully' });
+    }
+  
+});
+
+
+// Define a route to retrieve a particular video by filename
+app.get('/videos/:filename', (req, res) => {
+    const { filename } = req.params;
+    const filePath = path.join(__dirname, 'uploads', filename);
+  
+    // Check if the file exists
+    if (fs.existsSync(filePath)) {
+      const videoStream = fs.createReadStream(filePath);
+      res.setHeader('Content-Type', 'video/mp4'); // Adjust the content type as needed
+      videoStream.pipe(res);
+    } else {
+      res.status(404).json({ message: 'Video not found' });
+    }
+});
+  
+  // ...
+
+// ...
+
+// Define a route to render the HTML page for playing a video
+app.get('/play', (req, res) => {
+    const { filename } = req.query; // Get the video filename from the query parameters
+    if (!filename) {
+      res.status(400).json({ message: 'Video filename is required' });
+    } else {
+      res.sendFile(path.join(__dirname, 'play_video.html'));
+    }
+});
+  
+  // ...
+  
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
